e4b34ebfc45d478f02cf729c45c9ffa0a5d54cf0
JoshuaRosado <joshua06rosado@gmail.com>
2025-07-05 20:09:12 -0500

    Initial Commit

---
+++ KazokuFitTests/KazokuFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  KazokuFitTests.swift
+//  KazokuFitTests
+//
+//  Created by Joshua Rosado Olivencia on 7/5/25.
+//
+
+import Testing
+@testable import KazokuFit
+
+struct KazokuFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
